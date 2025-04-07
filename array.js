dfa439c9b9a0ee3f274873d8ba427132c82661a7
Mandeep0311 <k.mandeep43@gmail.com>
2025-04-07 14:57:17 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,45 @@
+let arr = []
+    arr.push(
+    {
+        song : "rehna hai tere dil me",
+        singer: "arijit singh",
+        language: "hindi"
+    }
+)
+arr.push(
+    {
+        song : "dhoom machale",
+        singer: "Sunidhi chohan",
+        language: "hindi"
+    }
+)
+arr.push(
+    {
+        song : "suraj hua madham",
+        singer: "Sonu Nigam",
+        language: "hindi"
+    }
+)
+
+arr.push(
+    {
+        song : "channa meriya",
+        singer: "Arijit",
+        language: "hindi"
+    }
+)
+arr.push(
+    {
+        song : "APT",
+        singer: "Rose",
+        language: "Korean"
+    }
+)
+
+console.log(arr);
+for (i=0; i<=arr.length; i++);
+
+console.log(arr[3].song);
+
+
+
