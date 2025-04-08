f28972b247d89a5c44a36a1489a574c7ba73d2c9
Mandeep0311 <k.mandeep43@gmail.com>
2025-04-08 14:50:47 +0530

    Add files via upload

---
+++ arr.js
@@ -0,0 +1,37 @@
+const { log } = require("console");
+
+var arr = ["aman","simran","ravi","Tina","arsh","karan"]
+   console.log(arr);
+{   arr.push("jasmine");
+   Console.log(arr)
+}
+{  
+   arr.unshift("joya")
+   log.console(arr);
+}
+{
+   arr.splice("2","3")
+   console.log(arr);
+   
+}
+{
+var arr1 = ["Anita","vijay"];
+console.log(arr1);
+}
+{
+   var x= arr.concat(arr1);
+   console.log(x);
+   
+}
+{
+   let x1= arr.map((el)=>
+   {
+      return "hello"+ el
+   })
+   console.log(x1);
+   }
+
+
+
+
+   
